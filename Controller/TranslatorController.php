5ae5278142f3e815b0802c53e65061c756663dfe
noureddine <n.elmoulaoui@gmail.com>
2013-09-02 14:15:29 +0200

    refactoring controller

--- Controller/TranslatorController.php
+++ Controller/TranslatorController.php
@@ -1,14 +1,12 @@
 <?php
 
 namespace Web\TranslatorBundle\Controller;
-
 use Web\TranslatorBundle\Entity\Translation;
 use Web\TranslatorBundle\Entity\Message;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
-
 
 class TranslatorController extends Controller
 {
@@ -31,14 +29,9 @@ class TranslatorController extends Controller
     public function showDomainsAction($bundle)
     {
         $target_bundles = $this->container->getParameter('translator_bundles');
-        $target_languages = $this->container
-                ->getParameter('translator_target_languages');
-        $target_domains = $this->container
-                ->getParameter('translator_target_domains');
 
         if (!isset($target_bundles[$bundle])) {
-            return $this
-                    ->forward('WebTranslatorBundle:Translator:showBundles');
+            return $this->forward('WebTranslatorBundle:Translator:showBundles');
         }
 
         $entries = $this->get('doctrine')
@@ -47,26 +40,14 @@ class TranslatorController extends Controller
 
         $domains = array();
 
-        $missing_entries = array();
-        foreach ($target_domains as $target_domain) {
-            $missing_entries[$target_domain] = 0;
-        }
-
         foreach ($entries as $specific_entry) {
             if (!in_array($specific_entry->getDomain(), $domains)) {
                 array_push($domains, $specific_entry->getDomain());
             }
-            //$message_collection = $specific_entry->getMessageCollection();
-            /*foreach($target_languages as $target_locale) {
-                if(!isset($message_collection[$target_locale])) {
-                    $missing_entries[$specific_entry->getDomain()]++;
-                }
-            }*/
-            //$missing_entries[$specific_entry->getDomain()] += count($target_languages) - count($specific_entry->getMessageCollection()) + 1;
         }
-        // Sort by value
+
         asort($domains);
-        //ksort($missing_entry);
+
         return $this
                 ->render(
                         'WebTranslatorBundle:Translator:show_domains.html.twig',
@@ -80,8 +61,7 @@ class TranslatorController extends Controller
                 || !in_array($domain,
                         $this->container
                                 ->getParameter('translator_target_domains'))) {
-            return $this
-                    ->forward('WebTranslatorBundle:Translator:showBundles');
+            return $this->forward('WebTranslatorBundle:Translator:showBundles');
         }
 
         $entries = $this->get('doctrine')
@@ -91,10 +71,9 @@ class TranslatorController extends Controller
                 ->getParameter('translator_target_languages');
         array_push($translator_languages,
                 $this->container->getParameter('translator_main_language'));
-        // For a bundle we want to show each translation_key and which languages it has a translation for
+
         $processed_entries = array();
         foreach ($entries as $specific_entry) {
-            //$processed_entries[$specific_entry->getTranslationKey()];
             $languages = array();
             foreach ($specific_entry->getMessages() as $message) {
                 $locale = $message->getLocale();
@@ -103,13 +82,14 @@ class TranslatorController extends Controller
             $processed_entries[$specific_entry->getTranslationKey()] = array_diff(
                     $translator_languages, $languages);
         }
+
         $target_locales = $this->container
                 ->getParameter('translator_target_languages');
-        // Sort by key (not value).
+
         ksort($processed_entries);
+
         return $this
-                ->render(
-                        'WebTranslatorBundle:Translator:show_keys.html.twig',
+                ->render('WebTranslatorBundle:Translator:show_keys.html.twig',
                         array('bundlename' => $bundle, 'domain' => $domain,
                                 'target_locales' => $target_locales,
                                 'entries' => $processed_entries));
@@ -125,8 +105,7 @@ class TranslatorController extends Controller
                 || !in_array($locale,
                         $this->container
                                 ->getParameter('translator_target_languages'))) {
-            return $this
-                    ->forward('WebTranslatorBundle:Translator:showBundles');
+            return $this->forward('WebTranslatorBundle:Translator:showBundles');
         }
 
         $entries = $this->get('doctrine')
@@ -159,8 +138,7 @@ class TranslatorController extends Controller
                 || !in_array($domain,
                         $this->container
                                 ->getParameter('translator_target_domains'))) {
-            return $this
-                    ->forward('WebTranslatorBundle:Translator:showBundles');
+            return $this->forward('WebTranslatorBundle:Translator:showBundles');
         }
 
         $entry_to_edit = $this->get('doctrine')
@@ -185,7 +163,6 @@ class TranslatorController extends Controller
 
         $main_message = $main_message->current();
 
-        // Need to add in the array all the languages
         foreach ($target_languages as $target_locale) {
             $message = $this->get('doctrine')
                     ->getRepository('WebTranslatorBundle:Message')
@@ -203,8 +180,7 @@ class TranslatorController extends Controller
         }
 
         return $this
-                ->render(
-                        'WebTranslatorBundle:Translator:show_key.html.twig',
+                ->render('WebTranslatorBundle:Translator:show_key.html.twig',
                         array('bundlename' => $bundle, 'domain' => $domain,
                                 'key' => $key,
                                 'main_language' => $main_language,
@@ -219,8 +195,7 @@ class TranslatorController extends Controller
                 || !in_array($domain,
                         $this->container
                                 ->getParameter('translator_target_domains'))) {
-            return $this
-                    ->forward('WebTranslatorBundle:Translator:showBundles');
+            return $this->forward('WebTranslatorBundle:Translator:showBundles');
         }
 
         $dm = $this->get('doctrine');
@@ -267,3 +242,4 @@ class TranslatorController extends Controller
                                 'key' => $key));
     }
 }
+
